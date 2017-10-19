fcbbb382a84d2b44c1a414387b1ab53c5556ab2e
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-10-19 14:21:54 -0400

    add #slug and .find_by_slug

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   validates :username, presence: true
   validates :first_name, presence: true
+
+  def slug
+    self.username.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.detect { |slug| slug == slug }
+  end
 end
