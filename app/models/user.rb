2ed50c592fa8dc7dabd1ad8ea55ca647848cedf0
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-10-19 14:16:58 -0400

    add validation and password macros to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
+  has_secure_password
   has_many :sequences
+  validates :email, presence: true
+  validates :username, presence: true
+  validates :first_name, presence: true
 end
