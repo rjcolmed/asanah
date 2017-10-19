78c31ec1cd21838fc9aa6da6ed6b7859f23a0105
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-10-19 15:46:38 -0400

    add get and post /login routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+
   get '/signup' do
     erb :'users/signup'
   end
@@ -14,7 +14,23 @@ class UsersController < ApplicationController
     else
       redirect '/users/signup'
     end
+  end
 
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+    binding.pry
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+
+      redirect "/users/#{user.slug}"
+    else
+      redirect '/users/login'
+    end
   end
 
   get '/users/:slug' do
