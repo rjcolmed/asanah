f49523d01cfdfc2ea9895278c4beef4995add0b3
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-10-19 15:33:49 -0400

    moved all user- or sequence-spec routes to their respective controller classes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,49 +13,4 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/signup' do
-    erb :'users/signup'
-  end
-
-  post '/signup' do
-    user = User.new(params)
-
-    if user.save
-      session[:user_id] = user.id
-
-      redirect "/users/#{user.slug}"
-    else
-      redirect '/users/signup'
-    end
-
-  end
-
-  get '/users/:slug' do
-    @user = User.find_by_slug(params[:slug])
-
-    erb :'users/show'
-  end
-
-  get '/sequences/new' do
-    erb :'sequences/new'
-  end
-
-  post '/sequences' do
-    binding.pry
-    sequence = Sequence.new(params)
-
-    if sequence.save
-      redirect "/sequences/#{sequence.slug}"
-    else
-      redirect 'sequences/new'
-    end
-
-  end
-
-  get '/sequences/:slug' do
-    @sequence = Sequence.find_by_slug(params[:slug])
-
-    erb :'sequences/show'
-  end
-
 end
