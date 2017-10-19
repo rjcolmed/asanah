9ea07ec426d1b937d21280bb73627b82d49711d0
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-10-19 14:12:02 -0400

    add post /signup route with basic user creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,17 @@ class ApplicationController < Sinatra::Base
     erb :'users/signup'
   end
 
+  post '/signup' do
+    user = User.new(params)
+    
+    if user.save
+      session[:user_id] = user.id
+
+      redirect '/tweets'
+    else
+      redirect '/users/signup'
+    end
+
+  end
+
 end
