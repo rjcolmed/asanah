88acb76d634930b0ba1ca141f990cfc9e6bd8c01
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-10-20 12:44:42 -0400

    make path more explicit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < Sinatra::Base
   include AuthenticationHelpers
 
   configure do
-    set :public_folder, 'public'
+    set :public_folder, '/public'
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "asanah_secret"
