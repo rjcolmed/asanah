414c9cb6eaacc634c5ea95bcc74250da492037bc
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-10-20 12:41:57 -0400

    moved from /public into public/scripts

---
+++ public/scripts/asanah.js
@@ -0,0 +1,15 @@
+// adds asana from pool to user list
+$('.asana').on('click', function() {
+  event.stopPropagation();
+  $(this).clone().appendTo($('.usr-sequence'));
+});
+
+// removes asana from user list
+$('.usr-sequence').on('click', '.asana', function() {
+  $(this).remove();
+});
+
+// removes asanas from user list when editing
+$('.list-asana').on('click', function() {
+  $(this).remove();
+});
