434877b10d5370116cc67cfab2523859e91ef873
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-10-20 14:46:24 -0400

    add .find_all_in

--- app/models/asana.rb
+++ app/models/asana.rb
@@ -2,4 +2,8 @@ class Asana < ActiveRecord::Base
   belongs_to :group
   has_many :sequence_asanas
   has_many :sequences, through: :sequence_asanas
+
+  def self.find_all_in(group)
+    self.where('group_id == ?', Group.find_by(name: group))
+  end
 end
