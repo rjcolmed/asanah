490e517c40eede3b06a1258d440c5df34577c18d
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-10-22 15:54:03 -0400

    added #logged_in and #current_user to validate user actions

--- app/controllers/sequences_controller.rb
+++ app/controllers/sequences_controller.rb
@@ -54,19 +54,28 @@ class SequencesController < ApplicationController
 
   get '/sequences/:slug/edit' do
     @sequence = Sequence.find_by_slug(params[:slug])
-    @grouped_asanas = Group.all_asanas_by_group
 
-    erb :'sequences/edit'
+    if logged_in? && current_user == @sequence.user
+      @grouped_asanas = Group.all_asanas_by_group
+        
+      erb :'sequences/edit'
+    else
+      redirect '/login'
+    end
+
   end
 
   patch '/sequences/:slug/edit' do
-
     sequence = Sequence.find_by_slug(params[:slug])
 
-    sequence.update(params[:sequence])
-    sequence.tap { |sequence| sequence.asana_ids = params[:asanas] }.save
+    if logged_in? && current_user == sequence.user
+      sequence.update(params[:sequence])
+      sequence.tap { |sequence| sequence.asana_ids = params[:asanas] }.save
+      redirect "/sequences/#{sequence.slug}"
+    else
+      redirect '/login'
+    end
 
-    redirect "/sequences/#{sequence.slug}"
   end
 
   delete '/sequences/:slug/delete' do
