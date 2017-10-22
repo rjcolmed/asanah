e031dc92df22fd5723bb72f8b39410a3024d4d0c
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-10-22 14:41:53 -0400

    add call to #save after adding asanas to sequence. refactor.

--- app/controllers/sequences_controller.rb
+++ app/controllers/sequences_controller.rb
@@ -52,7 +52,7 @@ class SequencesController < ApplicationController
     sequence = Sequence.find_by_slug(params[:slug])
 
     sequence.update(params[:sequence])
-    sequence.asana_ids = params[:asanas]
+    sequence.tap { |sequence| sequence.asana_ids = params[:asanas] }.save
 
     redirect "/sequences/#{sequence.slug}"
   end
