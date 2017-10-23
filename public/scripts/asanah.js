5384ec9dc923df5069ab0bac6b65732e27a5be16
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-10-23 11:12:28 -0400

    add semi-working jQuery allowing radio buttons to change text on items rendered in views

--- public/scripts/asanah.js
+++ public/scripts/asanah.js
@@ -15,3 +15,15 @@ $('.list-asana').on('click', function() {
 });
 
 $('.secondary.vertical.pointing.menu .item').tab();
+
+$('input[name="display"]').on('change', function() {
+  if ($(this).val() == 'english') {
+    $('input[type="hidden"]').each(function(){
+      this.nextSibling.textContent = '<%= asana.english_name %>';
+    });
+  } else {
+    $('input[type="hidden"]').each(function(){
+      this.nextSibling.textContent = '<%= asana.transliterated_name %> (<%= asana.devanagari_name %>)';
+    });
+  }
+});
