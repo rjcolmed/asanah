9b82ac4c44080e3cd1266b45e68ed8b729545e95
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-10-27 15:49:38 -0400

    add more descriptive notes to each jQuery function and new function allowing users to toggle between asana display language

--- public/scripts/asanah.js
+++ public/scripts/asanah.js
@@ -1,19 +1,27 @@
-// adds asana from pool to user list
+// adds asana from pool to user list when user clicks
 $('.asana').on('click', function() {
   event.stopPropagation();
   $(this).clone().appendTo($('.usr-sequence'));
 });
 
-// removes asana from user list
+// removes asana from user list when user clicks
 $('.usr-sequence').on('click', '.asana', function() {
   $(this).remove();
 });
 
-// removes asanas from user list when editing
+// removes asanas from user list when user clicks
 $('.list-asana').on('click', function() {
   $(this).remove();
 });
 
+// allows tabs to show connected content when user clicks on each tab 
 $('.secondary.vertical.pointing.menu .item').tab();
 
+// allows user to toggle between displaying asana names in english and sanskrit
+$('input[type="radio"]').on('change', function(){
+
+      $('div#english').toggleClass('hidden')
+      $('div#translit-devanagari').toggleClass('hidden')
+
+});
 
